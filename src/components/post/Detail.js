d9bd9e17adf5fcd65008bf4bdefa42a292ab0a15
jeongogo <chcgogo2@naver.com>
2022-04-25 11:07:12 +0900

    image upload

--- src/components/post/Detail.js
+++ src/components/post/Detail.js
@@ -14,46 +14,43 @@ const PostDetail = ({ post, goBack, goEdit }) => {
 
   return (
     <div className="px-4 py-10">
-      {thumbnail && 
-        <div>
-          <img src={`/upload/${thumbnail}`} alt="" />
-        </div>
-      }
-      <h2 className="mb-4 text-center text-xl font-bold">{title}</h2>
-      <div className="px-6 py-6 shadow-3xl rounded-3xl">
-        <ul>
-          <li className="py-3">
-            <h3 className="mr-6 shrink-0 w-20 underline">재료</h3>
-            <p className="mt-2">{material}</p>
-          </li>
-          <li className="py-3">
-            <h3 className="mr-6 shrink-0 w-20 underline">양념</h3>
-            <p className="mt-2">{seasoning}</p>
-          </li>
-          <li className="py-3">
-            <h3 className="mr-6 shrink-0 w-20 underline">조리 순서</h3>
-            <p className="mt-2">{Parser(content)}</p>
-          </li>
-        </ul>
-        <div className="flex justify-end text-xs text-gray-400">
-          Chef. <span className="hover:underline">{author.nickname}</span>
-        </div>
-        <div className="flex justify-center items-center mt-8">
-          <div className="ml-4">
-            <button
-              type="button"
-              className="py-2 px-6 mr-2 text-sm font-medium rounded-3xl bg-gray-200"
-              onClick={onGoBack}
-            >
-              뒤로가기
-            </button>
-            <button
-              type="button"
-              className="py-2 px-6 text-sm font-medium rounded-3xl bg-yellow-400"
-              onClick={onGoEdit}
-            >
-              수정하기
-            </button>
+      <div className="max-w-xl ml-auto mr-auto">
+        <h2 className="mb-4 text-center text-xl font-bold">{title}</h2>
+        <div className="px-6 py-6 shadow-3xl rounded-3xl">
+          <ul>
+            <li className="py-3">
+              <h3 className="mr-6 shrink-0 w-20 underline">재료</h3>
+              <p className="mt-2">{material}</p>
+            </li>
+            <li className="py-3">
+              <h3 className="mr-6 shrink-0 w-20 underline">양념</h3>
+              <p className="mt-2">{seasoning}</p>
+            </li>
+            <li className="py-3">
+              <h3 className="mr-6 shrink-0 w-20 underline">조리 순서</h3>
+              <p className="mt-2">{Parser(content)}</p>
+            </li>
+          </ul>
+          <div className="flex justify-end text-xs text-gray-400">
+            Chef. <span className="hover:underline">{author.nickname}</span>
+          </div>
+          <div className="flex justify-center items-center mt-8">
+            <div className="ml-4">
+              <button
+                type="button"
+                className="py-2 px-6 mr-2 text-sm font-medium rounded-3xl bg-gray-200"
+                onClick={onGoBack}
+              >
+                뒤로가기
+              </button>
+              <button
+                type="button"
+                className="py-2 px-6 text-sm font-medium rounded-3xl bg-yellow-400"
+                onClick={onGoEdit}
+              >
+                수정하기
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/post/List.js
+++ src/components/post/List.js
@@ -2,15 +2,20 @@ import { Link } from "react-router-dom";
 
 const List = ({ data }) => {
   return (
-    <div className="px-4 py-8">
-      <ul className="grid grid-cols-2 gap-2">
+    <div className="md:px-4 md:py-8">
+      <ul className="max-w-2xl ml-auto mr-auto grid grid-cols-2 md:grid-cols-3 gap-1 md:gap-2">
         {data.map((post) => (
           <li key={post._id}>
             <Link
               to={`/posts/detail/${post._id}`}
-              className="block text-center"
+              className="h-28 flex items-center justify-center bg-gray-50 overflow-hidden"
             >
-              {post.title}
+            {post.thumbnail
+              ? 
+                <img src={`/upload/${post.thumbnail}`} alt="" className="w-full" />
+              :
+                <h2>{post.title}</h2>
+              }
             </Link>
           </li>
         ))}
