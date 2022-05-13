1993aef147ef9a5e0a265f12218084530fb4f4ac
jeongogo <chcgogo2@naver.com>
2022-05-13 15:56:11 +0900

    kakao

--- src/containers/Run/Chart.container.js
+++ src/containers/Run/Chart.container.js
@@ -4,9 +4,34 @@ import * as Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
 const ChartContainer = () => {
+  const url = window.location.href;
   const [teamJ, setTeamJ] = useState();
   const [teamP, setTeamP] = useState();
   const [runList, setRunList] = useState([]);
+  
+  const initKakao = () => {
+    if (window.Kakao) {
+      const kakao = window.Kakao;
+      if (!kakao.isInitialized()) {
+        kakao.init('0eb1170dc2d4792fec250441ed023311');
+      }
+    }
+  };
+
+  const shareKakao = () => {
+    window.Kakao.Link.sendDefault({ 
+      objectType: 'feed',
+      content: {
+        title: '제5회 레이크러너 팀전 레이스 대회',
+        description: `J ${teamJ} vs P ${teamP}`,
+        imageUrl: 'http://mongchan.com/mbti.jpg',
+        link: {
+          mobileWebUrl: url,
+          webUrl: url,
+        },
+      },
+    });
+  };    
 
   const getList = async () => {
     const res = await client('/api/run');
@@ -25,6 +50,7 @@ const ChartContainer = () => {
   
   useEffect(() => {
     getList();
+    initKakao();
     document.title = "제5회 레이크러너 팀전 레이스 대회"
   }, []);
 
@@ -53,34 +79,35 @@ const ChartContainer = () => {
       }
     },
     colors: [
-      '#BBDEFB',
+      '#EF9A9A',
+      '#F48FB1',
       '#90CAF9',
-      '#B3E5FC',
-      '#81D4FA',
-      '#B2EBF2',
-      '#80DEEA',
-      '#B2DFDB',
       '#80CBC4',
+      '#BBDEFB',      
+      '#F8BBD0',
+      '#EF9A9A',      
+      '#CE93D8',
       '#C8E6C9',
       '#A5D6A7',
-      '#BBDEFB',
-      '#90CAF9',
-      '#B3E5FC',
-      '#81D4FA',
-      '#FFCDD2',
-      '#EF9A9A',
-      '#F8BBD0',
       '#F48FB1',
-      '#E1BEE7',
-      '#CE93D8',
-      '#FFCCBC',
       '#FFAB91',
+      '#B3E5FC',      
       '#FFE0B2',
+      
+      '#FFCDD2',
+      '#B2DFDB',
+      '#80DEEA',
+      '#B2EBF2',
+      '#E1BEE7',
+      '#81D4FA',
+      '#FFCCBC',
+      '#B3E5FC',
+      '#90CAF9',
       '#FFCC80',
       '#F8BBD0',
-      '#F48FB1',
+      '#81D4FA',
       '#FFCDD2',
-      '#EF9A9A',
+      '#BBDEFB',
     ],
     series: runList
   };  
@@ -89,14 +116,17 @@ const ChartContainer = () => {
     <div className='px-4 py-4'>
       <HighchartsReact highcharts={ Highcharts } options={ options }/>
       <div className='flex justify-center text-center mt-10'>
-        <div className='px-10 py-6 shadow-3xl rounded-3xl mx-6'>
+        <div className='px-10 py-6 shadow-3xl rounded-3xl mx-4'>
           <h3>Team J</h3>
           <p className='mt-1 font-bold text-2xl'>{teamJ}</p>
         </div>
-        <div className='px-10 py-6 shadow-3xl rounded-3xl mx-6'>
+        <div className='px-10 py-6 shadow-3xl rounded-3xl mx-4'>
           <h3>Team P</h3>
           <p className='mt-1 font-bold text-2xl'>{teamP}</p>
         </div>
+      </div>
+      <div className='flex justify-center text-center mt-10 fixed bottom-0 left-0 w-full'>
+        <button type='button' className='w-full py-4 font-medium' style={{'backgroundColor': '#f3dc00', 'color': '#391d1d'}} onClick={shareKakao}>카카오톡으로 공유하기</button>
       </div>
     </div>
   )
